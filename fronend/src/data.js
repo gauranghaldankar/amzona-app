ee100851009ed4f275806af192a0f195bae62516
gauranghaldankar <gaurang.haldankar@gmail.com>
2021-07-17 18:54:34 +0530

    share code on github

---
+++ fronend/src/data.js
@@ -0,0 +1,38 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Hikari Marine S',
+      category: 'Fish Food',
+      image: '/images/Hikari-Marine-S_nbtgvq.webp',
+      price: 350,
+      brand: 'Hikari',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      _id: '2',
+      name: 'Hikari Marine S',
+      category: 'Marine Salt',
+      image: '/images/Red-Sea-Coral-Pro_qsxodb.jpg',
+      price: 3500,
+      brand: 'Red Sea',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      _id: '3',
+      name: 'Hikari Marine S',
+      category: 'Marine Salt',
+      image: '/images/Red-Sea-Salt_sbozdb.webp',
+      price: 3500,
+      brand: 'Red Sea',
+      rating: 5.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
