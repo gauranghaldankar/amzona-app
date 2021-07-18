b4db59e7002e09ef14b5bdcf442f1555b45beb6d
gauranghaldankar <gaurang.haldankar@gmail.com>
2021-07-18 10:30:56 +0530

    1. Added home screen and product screen 2. Added countInStock 3. app.js we imported browser router and route from react-router-dom 4. We added 2 route for HomeScreen and Product. 5. We added the styling in the list, button and row and columns

--- fronend/src/components/Product.js
+++ fronend/src/components/Product.js
@@ -9,14 +9,14 @@ export default function Product(props) {
         <img className="medium" src={product.image} alt={product.name} />
       </a>
       <div className="card-body">
-        <a href="product.html">
+        <a href={`/product/${product._id}`}>
           <h2>{product.name}</h2>
         </a>
         <Rating
           rating={product.rating}
           numReviews={product.numReviews}
         ></Rating>
-        <div className="price">Rs.{product.price}</div>
+        <div className="price">₹{product.price}</div>
       </div>
     </div>
   );
