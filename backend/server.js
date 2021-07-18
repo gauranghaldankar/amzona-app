a10dd766af04d6fc0449b23e6e58c1be3fd055e9
gauranghaldankar <gaurang.haldankar@gmail.com>
2021-07-18 23:45:42 +0530

    PayPal added

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -10,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// eslint-disable-next-line no-undef
+
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -20,6 +21,9 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
@@ -29,7 +33,6 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-// eslint-disable-next-line no-undef
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
