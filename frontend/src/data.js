1fc1112c7b97df81608a008f386f437d58825dfa
gauranghaldankar <gaurang.haldankar@gmail.com>
2021-07-18 11:26:06 +0530

    Load Products From Backend     1. edit HomeScreen.js     2. define products, loading and error.     3. create useEffect     4. define async fetchData and call it     5. install axios     6. get data from /api/products     7. show them in the list     8. create Loading Component     9. create Message Box Component     10. use them in HomeScreen

---
+++ frontend/src/data.js
@@ -0,0 +1,41 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Hikari Marine S',
+      category: 'Fish Food',
+      image: '/images/Hikari-Marine-S_nbtgvq.webp',
+      price: 350,
+      countInStock: 20,
+      brand: 'Hikari',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      _id: '2',
+      name: 'Red Sea Coral Pro Salt',
+      category: 'Marine Salt',
+      image: '/images/Red-Sea-Coral-Pro_qsxodb.jpg',
+      price: 3500,
+      countInStock: 0,
+      brand: 'Red Sea',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      _id: '3',
+      name: 'Red Sea Salt',
+      category: 'Marine Salt',
+      image: '/images/Red-Sea-Salt_sbozdb.webp',
+      price: 3500,
+      countInStock: 5,
+      brand: 'Red Sea',
+      rating: 5.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
