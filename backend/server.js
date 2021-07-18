6737ad532198b0125c8e569880256d6c09f6fbdb
gauranghaldankar <gaurang.haldankar@gmail.com>
2021-07-18 18:33:25 +0530

    Generate JSON Token and signin method

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 
+dotenv.config();
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // eslint-disable-next-line no-undef
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
   useNewUrlParser: true,
@@ -17,6 +22,7 @@ app.get('/', (req, res) => {
   res.send('Server is ready');
 });
 
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
